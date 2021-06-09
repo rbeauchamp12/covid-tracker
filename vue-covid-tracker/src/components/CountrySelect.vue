1c69aab08e9e9d375a3808d5f70095dcadae9c59
Robert Beauchamp <robthedeveloper12@gmail.com>
2021-06-09 03:21:56 -0400

    .

--- vue-covid-tracker/src/components/CountrySelect.vue
+++ vue-covid-tracker/src/components/CountrySelect.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <select @change="onChange" v-modal="selected" class="form-select mt-10 block w-full border p-3 rounded">
+        <option value="0"> Select Country </option>
+        <option v-for="country in countries" :value="country.ID">{{ country.Country}} </option>
+
+
+
+    </select>
+
 </template>
 
 <script>
@@ -7,5 +14,18 @@
 export default {
     name: 'CountrySelect',
     props: ['countries'],
+    data() {
+        return {
+            select: 0
+        }
+    },
+
+    methods: {
+        onChange() {
+            const country = this.countries.find((item) =>item.ID === this.selected)
+
+            this.$emit('get-country', country)
+        },
+    },
 }
 </script>
--- vue-covid-tracker/src/views/Home.vue
+++ vue-covid-tracker/src/views/Home.vue
@@ -2,6 +2,9 @@
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats"  />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button @click="clearCountryData" v-if="stats.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">Clear Country</button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -33,7 +36,7 @@ export default {
       stats: {},
       countries: [],
       loadingImage: require('../assets/hourglass.gif')
-  }
+    }
 },
   methods: {
     async fetchCovidData() {
@@ -41,7 +44,19 @@ export default {
       const data = await res.json()
       return data
       },
-  },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country  
+    },
+
+   async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
+    },
+},
   async created() {
     const data = await this.fetchCovidData()
 
