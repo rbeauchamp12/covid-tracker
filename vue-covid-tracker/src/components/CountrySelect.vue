1c6834bb4ddb5fab404486dd852aa818f5b9b91f
Robert Beauchamp <robthedeveloper12@gmail.com>
2021-06-10 20:29:55 -0400

    .

--- vue-covid-tracker/src/components/CountrySelect.vue
+++ vue-covid-tracker/src/components/CountrySelect.vue
@@ -2,7 +2,7 @@
     <select @change="onChange()" v-modal="selected" class="form-select mt-10 block w-full border p-3 rounded">
         <option value="0"> Select Country </option>
 
-        <option v-for="country in countries" :value="country.ID"> {{ country.Country }} </option>
+        <option v-for ="country in countries" :value="country.ID"> {{ country.Country }} </option>
 
     </select>
 </template>
